f1e4ebb6b51012c94e5956890a3d04378b7cc759
toki shono <toki.shigoto@gmail.com>
2023-09-26 07:25:48 +0900

    addmenuのクローズ機能を追加

--- assets/common.js
+++ assets/common.js
@@ -1,8 +1,19 @@
 const $addMenu = document.getElementById("js-addMenu");
+const $closeButtun = document.querySelector(".close__btn");
 const $addTrigger = document.getElementById("js-addTrigger");
 const $mask = document.getElementById("mask");
 
 $addTrigger.addEventListener("click", function () {
   $addMenu.classList.add("open");
   $mask.classList.add("open");
+});
+
+$closeButtun.addEventListener("click", function () {
+  $addMenu.classList.remove("open");
+  $mask.classList.remove("open");
+});
+
+$mask.addEventListener("click", () => {
+  $addMenu.classList.remove("open");
+  $mask.classList.remove("open");
 });
