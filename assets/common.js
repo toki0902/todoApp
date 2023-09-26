bcf647d1920917249e4681a74bad1c8f72d29743
Toki Shono <toki.shigoto@gmail.com>
2023-09-26 14:50:47 +0900

    カラー選択機能実装中]

--- assets/common.js
+++ assets/common.js
@@ -2,6 +2,9 @@ const $addMenu = document.getElementById("js-addMenu");
 const $closeButtun = document.querySelector(".close__btn");
 const $addTrigger = document.getElementById("js-addTrigger");
 const $mask = document.getElementById("mask");
+const $color = document.querySelectorAll(".color__item");
+const $label = document.getElementsByTagName("label");
+
 
 $addTrigger.addEventListener("click", function () {
   $addMenu.classList.add("open");
@@ -17,3 +20,13 @@ $mask.addEventListener("click", () => {
   $addMenu.classList.remove("open");
   $mask.classList.remove("open");
 });
+
+for(let i=1;i < $color.length + 1; i++){
+  $color[i - 1].addEventListener("click" , ()=>{
+    let getColor = getComputedStyle($color[i - 1]).backgroundColor;
+    getComputedStyle($label[0]).color = (getColor);
+    getComputedStyle($label[1]).color = (getColor);
+});
+};
+
+
